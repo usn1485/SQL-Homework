8f971683614d0a4a82843e0d0b967540d87ebd6a
Ujwala <contact.uju@gmail.com>
2019-06-20 12:02:10 -0500

    Completed Query work

--- HPHW.sql
+++ HPHW.sql
@@ -1,59 +1,54 @@
 --Select Queries
-Select * from employees;
-Select * from departments;
-Select * from dept_emp;
-Select * from dept_manager;
-Select * from salaries;
-Select * from titles;
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
 
---1. List the following details of each employee: employee number, last name, first name, gender, and salary.
---SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
---FROM Orders
---INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+--1. List the following details of each employee: employee number, lASt name, first name, gender, and salary.
 
-Select e.emp_no, e.last_name, e.first_name, e.gender, s.salary from employees as e
-INNER JOIN  salaries as s ON s.emp_no=e.emp_no;
-
-
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary FROM employees AS e
+INNER JOIN  salaries AS s ON s.emp_no=e.emp_no;
 
 --2. List employees who were hired in 1986.
 
-select emp_no, first_name, last_name, hire_date from employees
-where hire_date between '1986-01-01' AND '1987-01-01';
+SELECT emp_no, first_name, last_name, hire_date FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
 
 --3. List the manager of each department with the following information: department number, department name,
---the manager's employee number, last name, first name, and start and end employment dates.
+--the manager's employee number, lASt name, first name, and start and end employment dates.
 
-Select d.dept_no, d.dept_name,e.emp_no, e.last_name, e.first_name,dm.from_date, dm.to_date from employees as e
-INNER JOIN  dept_manager as dm ON dm.emp_no=e.emp_no
-INNER JOIN departments as d on dm.dept_no = d.dept_no ;
+SELECT d.dept_no, d.dept_name,e.emp_no, e.last_name, e.first_name,dm.FROM_date, dm.to_date FROM employees AS e
+INNER JOIN  dept_manager AS dm ON dm.emp_no=e.emp_no
+INNER JOIN departments AS d ON dm.dept_no = d.dept_no ;
 
---4. List the department of each employee with the following information: 
---employee number, last name, first name, and department name.
-select de.emp_no,e.last_name, e.first_name, d.dept_name from employees as e
-INNER JOIN dept_emp as de ON de.emp_no=e.emp_no
-INNER JOIN departments as d on de.dept_no = d.dept_no ;
+--4. List the department of each employee with the following informatiON: 
+--employee number, lASt name, first name, and department name.
+SELECT de.emp_no,e.last_name, e.first_name, d.dept_name FROM employees AS e
+INNER JOIN dept_emp AS de ON de.emp_no=e.emp_no
+INNER JOIN departments AS d ON de.dept_no = d.dept_no ;
 
---5. List all employees whose first name is "Hercules" and last names begin with "B."
+--5. List all employees whose first name is "Hercules" and lASt names begin with "B."
 
-select e.first_name, e.last_name from employees as e where e.first_name = 'Hercules' AND e.last_name like 'B%';
+SELECT e.first_name, e.last_name FROM employees AS e WHERE e.first_name = 'Hercules' AND e.last_name LIKE 'B%';
 
---6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+--6. List all employees in the Sales department, including their employee number, lASt name, first name, and department name.
 
-select e.emp_no,e.last_name, e.first_name, d.dept_name from employees as e 
-INNER JOIN dept_emp as de ON de.emp_no=e.emp_no
-INNER JOIN departments as d on de.dept_no = d.dept_no where dept_name='Sales';
+SELECT e.emp_no,e.last_name, e.first_name, d.dept_name FROM employees AS e 
+INNER JOIN dept_emp AS de ON de.emp_no=e.emp_no
+INNER JOIN departments AS d ON de.dept_no = d.dept_no WHERE dept_name='Sales';
 
 --7. List all employees in the Sales and Development departments, 
---including their employee number, last name, first name, and department name.
+--including their employee number, lASt name, first name, and department name.
 
-select e.emp_no,e.last_name, e.first_name, d.dept_name from employees as e 
-INNER JOIN dept_emp as de ON de.emp_no=e.emp_no
-INNER JOIN departments as d on de.dept_no = d.dept_no where dept_name IN ('Sales','Development') ;
+SELECT e.emp_no,e.last_name, e.first_name, d.dept_name FROM employees AS e 
+INNER JOIN dept_emp AS de ON de.emp_no=e.emp_no
+INNER JOIN departments AS d ON de.dept_no = d.dept_no WHERE dept_name IN ('Sales','Development') ;
 
---8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-select last_name,count(last_name) as "Last Name count" from employees 
-group by last_name 
-order by  "Last Name count" desc;
+--8. In descending order, list the frequency count of employee lASt names, i.e., how many employees share each lASt name.
+SELECT last_name,COUNT(last_name) AS "Last Name count" FROM employees 
+GROUP BY last_name 
+ORDER BY  "Last Name count" DESC;
 
 
---
+++ databaseconfig.py
@@ -0,0 +1,2 @@
+#Setting up the postgress password
+hp_pwd="password"
